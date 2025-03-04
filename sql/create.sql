837577ca7eb16ca17e906b574491616df29e27a0
IceRinne <1024622242@qq.com>
2025-03-04 16:57:29 +0800

    init

--- sql/create.sql
+++
@@ -1,6 +0,0 @@
-create database scMoAnnoDB;
-use scMoAnnoDB;
-
-CREATE USER 'scmoannoadmin'@'%' IDENTIFIED BY '114514';
-GRANT ALL PRIVILEGES ON scMoAnnoDB.* TO 'scmoannoadmin'@'%' WITH GRANT OPTION;
-
